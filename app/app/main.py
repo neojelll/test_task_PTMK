5b69a534748afd571575460693018b5849844b7c
deadonted <timofejmitusin60892@gmail.com>
2024-10-19 23:04:44 +0300

    refactor: carried out optimization in dal'

--- app/app/main.py
+++ app/app/main.py
@@ -2,24 +2,7 @@ import random
 from datetime import datetime, timedelta
 from sqlalchemy import create_engine, Column, Integer, String , Date, func
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-
-Base = declarative_base()
-
-
-class Employee(Base):
-	__tablename__ = "employees"
-	id = Column(Integer, primary_key=True)
-	full_name = Column(String, nullable=False)
-	birth_date = Column(Date, nullable=False)
-	gender = Column(String, nullable=False)
-
-	def age(self):
-		return (datetime.now().date() - self.birth_date).days // 365
-	
-	def save(self, session):
-		session.add(self)
-		session.commit()
+from dal import Employee, EmployeeDAL, Base
 
 
 def create_connection():
@@ -67,7 +50,5 @@ def query_male_f_employees(session):
 
 
 def optimize_and_query_male_f_employees(session):
-    with session.begin():
-        session.execute("CREATE INDEX IF NOT EXISTS idx_gender ON employees(gender);")
-        session.execute("CREATE INDEX IF NOT EXISTS idx_full_name ON employees(full_name);")
+    EmployeeDAL(session).optimize()
     query_male_f_employees(session)
